c081cdbe6d644b908596fa645bd91789d51f960d
Alexey Artsukevich <a.artsukevich@rain.ru>
2020-09-02 16:31:03 +0300

    add optimizer Lamb and AdaLambW

--- deepl/utils/optimizer.py
+++ deepl/utils/optimizer.py
@@ -272,8 +272,8 @@ class AdaSGDW(Optimizer):
     def __init__(
         self,
         params: Iterable[torch.nn.parameter.Parameter],
-        lr: float = 1e-3,
-        betas: Tuple[float, float] = (0.9, 0.999),
+        lr: float = 1e-5,
+        betas: Tuple[float, float] = (0.999, 0.9),
         eps: float = 1e-6,
         weight_decay: float = 0.0
     ):
@@ -345,5 +345,200 @@ class AdaSGDW(Optimizer):
         """
         for group in self.param_groups:
             for p in group['params']:
+                if p.grad is not None:
+                    p.grad = None
+
+
+class Lamb(Optimizer):
+    r"""source: https://github.com/cybertronai/pytorch-lamb/blob/master/pytorch_lamb/lamb.py
+    Implements Lamb algorithm.
+    It has been proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes`_.
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+    .. _Large Batch Optimization for Deep Learning: Training BERT in 76 minutes:
+        https://arxiv.org/abs/1904.00962
+    """
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6,
+                 weight_decay=0):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay)
+        super(Lamb, self).__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('Lamb does not support sparse gradients.')
+
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                # Paper v3 does not use debiasing.
+                # bias_correction1 = 1 - beta1 ** state['step']
+                # bias_correction2 = 1 - beta2 ** state['step']
+                # Apply bias to lr to avoid broadcast.
+                step_size = group['lr'] # * math.sqrt(bias_correction2) / bias_correction1
+
+                weight_norm = p.data.pow(2).sum().sqrt()
+
+                adam_step = exp_avg / exp_avg_sq.sqrt().add(group['eps'])
+                if group['weight_decay'] != 0:
+                    adam_step.add_(group['weight_decay'], p.data)
+
+                adam_norm = adam_step.pow(2).sum().sqrt()
+                if weight_norm == 0 or adam_norm == 0:
+                    trust_ratio = 1
+                else:
+                    trust_ratio = weight_norm / adam_norm
+
+                p.data.sub_(adam_step, alpha=step_size * trust_ratio)
+
+        return loss
+
+    def zero_grad(self):
+        """Clears the gradients of all optimized :class:`torch.Tensor` s.
+        PyTorch Performance Tuning Guide - Szymon Migacz, NVIDIA:
+            * doesn't execute memset for every parameter
+            * memory is zeroed-out by the allocator in a more efficient way
+            * backward pass updates gradients with "=" operator (write)
+        """
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is not None:
+                    p.grad = None
+
+
+class AdaLambW(Optimizer):
+    r"""Experimental modification of Lamb.
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+    .. _Large Batch Optimization for Deep Learning: Training BERT in 76 minutes:
+        https://arxiv.org/abs/1904.00962
+    """
+
+    def __init__(self, params, lr=1e-5, betas=(0.999, 0.9), eps=1e-6,
+                 weight_decay=0):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay)
+        super().__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError("AdaLambW does not support sparse gradients.")
+
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state["step"] = 0
+                    state["exp_avg"] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    state["exp_avg_sq"] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                beta1, beta2 = group["betas"]
+
+                state["step"] += 1
+
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
+                denom = exp_avg_sq.sqrt().add_(group["eps"])
+
+                bias_correction2 = 1.0 - beta2 ** state["step"]
+                step_size = group["lr"] * math.sqrt(bias_correction2)
+                exp_avg.mul_(beta1).addcdiv_(grad, denom,
+                                             value=(1.0 - beta1) * step_size)
+
+                weight_norm = p.data.pow(2).sum().sqrt()
+                alg_norm = exp_avg.pow(2).sum().sqrt()
+
+                if weight_norm == 0 or alg_norm == 0:
+                    trust_ratio = 1
+                else:
+                    trust_ratio = weight_norm / alg_norm
+                p.data.sub_(exp_avg, alpha=trust_ratio)
+
+                if group["weight_decay"] > 0.0:
+                    p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
+
+        return loss
+
+    def zero_grad(self):
+        """Clears the gradients of all optimized :class:`torch.Tensor` s.
+        PyTorch Performance Tuning Guide - Szymon Migacz, NVIDIA:
+            * doesn't execute memset for every parameter
+            * memory is zeroed-out by the allocator in a more efficient way
+            * backward pass updates gradients with "=" operator (write)
+        """
+        for group in self.param_groups:
+            for p in group["params"]:
                 if p.grad is not None:
                     p.grad = None
