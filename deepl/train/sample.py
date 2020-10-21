e5486135a2153af60faa18e848f11bfa654e332f
Alexey Artsukevich <artsukevich@gmail.com>
2020-10-21 14:56:38 +0300

    improve sample functions

--- deepl/train/sample.py
+++ deepl/train/sample.py
@@ -1,38 +1,47 @@
 import numpy as np
 import random
+import torch
 
 
-def random_token_sample(tokens, *args):
-    return random.randrange(len(tokens))
+def random_token_sample(tokens, size=1, max_iter=None):
+    if max_iter is None:
+        max_iter = 10 * size
+    ids, cnt = set(), 0
+    while len(ids) < size and cnt < max_iter:
+        ids.add(random.randrange(len(tokens)))
+        cnt += 1
+    return list(ids)
 
 
 class InverseFreqSample:
-    def __init__(self, alpha=0.5):
+    def __init__(self, token_count, alpha=0.5):
         self.alpha = alpha
+        self.token_count = token_count
 
-    def __call__(self, tokens, tokens_count, *args):
-        tokens_count = np.asarray(tokens_count)
-        tokens_count = 1.0 / tokens_count
-        tokens_count = np.power(tokens_count, self.alpha)
-        tokens_count /= np.sum(tokens_count)
-        tokens_count = np.cumsum(tokens_count)
-        tokens_count = np.insert(tokens_count, 0, 0.0)
-        u = np.random.uniform()
-        for i in range(len(tokens_count)-1):
-            v1 = tokens_count[i]
-            v2 = tokens_count[i+1]
-            if v1 <= u < v2:
-                return i
-        return len(tokens_count) - 1
+    def __call__(self, tokens, size=1, max_iter=None):
+        if max_iter is None:
+            max_iter = 10 * size
+        proba = torch.tensor([self.token_count[w] for w in tokens],
+                             dtype=torch.float32)
+        proba = torch.pow(proba, -self.alpha)
+        sampler = torch.distributions.categorical.Categorical(proba)
+        ids, cnt = set(), 0
+        while len(ids) < size and cnt < max_iter:
+            for i in sampler.sample((size, )).tolist():
+                ids.add(i)
+                if len(ids) >= size:
+                    break
+            cnt += 1
+        return list(ids)
 
 
-def mask_token(tokens, token_count, id_mask, id_ignore,
+def mask_token(tokens, id_mask, id_ignore,
                sampler=random_token_sample):
     sequence, labels = [], []
-    for b, c in zip(tokens, token_count):
+    for b in tokens:
         s = b[:]
         v = [id_ignore] * len(s)
-        i = sampler(b, c)
+        i = sampler(b, size=1)[0]
         v[i] = s[i]
         s[i] = id_mask
         sequence.append(s)
@@ -40,43 +49,41 @@ def mask_token(tokens, token_count, id_mask, id_ignore,
     return sequence, labels
 
 
-def mask_token_with_unity_mlm(tokens, token_count, proba_unity, proba_token,
+def mask_token_with_unity_mlm(tokens, proba_unity, proba_token,
                               id_mask, id_ignore, sampler=random_token_sample):
     sequence, labels, masked_pos = [], [], []
-    for b, c in zip(tokens, token_count):
+    for b in tokens:
         s = b[:]
         u = np.random.uniform()
         if u < proba_unity:
             v = s[:]
-            i_to_mask = set()
+            i_to_mask = []
         else:
             v = [id_ignore] * len(s)
             num_token_to_mask = np.random.binomial(len(b), proba_token)
             num_token_to_mask = max(1, num_token_to_mask)
             num_token_to_mask = min(num_token_to_mask, len(s))
-            i_to_mask = set()
-            while len(i_to_mask) < num_token_to_mask:
-                i_to_mask.add(sampler(b, c))
+            i_to_mask = sampler(b, size=num_token_to_mask)
             for i in i_to_mask:
                 v[i] = s[i]
                 s[i] = id_mask
         sequence.append(s)
         labels.append(v)
-        masked_pos.append(list(i_to_mask))
+        masked_pos.append(i_to_mask)
     return sequence, labels, masked_pos
 
 
-def mask_token_with_unity_lm(tokens, token_count, proba_unity,
+def mask_token_with_unity_lm(tokens, proba_unity,
                              id_sos, id_eos, id_ignore,
                              sampler=random_token_sample):
     sequence, labels = [], []
-    for b, c in zip(tokens, token_count):
+    for b in tokens:
         u = np.random.uniform()
         if u < proba_unity:
             s = [id_sos] + b
             v = b + [id_eos]
         else:
-            n = sampler(b, c)
+            n = sampler(b, size=1)[0]
             s = [id_sos] + b[:n]
             v = [id_ignore] * len(s)
             v[n] = b[n]
