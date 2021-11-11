f1196ca0f7fe04664620e848f0c8abfd4c0f7ac0
Alexey Artsukevich <a.artsukevich@rain.ru>
2021-11-11 17:03:59 +0300

    loss collect function for early stopping forward

--- deepl/train/utils.py
+++ deepl/train/utils.py
@@ -8,3 +8,26 @@ def kld_loss(module, method_name, method_kwargs=None):
     for child in module.children():
         loss += kld_loss(child, method_name, method_kwargs)
     return loss
+
+
+def kld_loss_nan(module, method_name, method_kwargs=None):
+    """Функция необходима для случая когда не все слои модели
+    работают."""
+    if method_kwargs is None:
+        method_kwargs = {}
+    loss = []
+    if hasattr(module, method_name):
+        try:
+            loss += [getattr(module, method_name)(**method_kwargs)]
+        except Exception:
+            pass
+    for child in module.children():
+        loss += kld_loss_nan(child, method_name, method_kwargs)
+    return loss
+
+
+def reset_module_loss(module, method_name, value_name):
+    if hasattr(module, method_name):
+        setattr(module, value_name, None)
+    for child in module.children():
+        reset_module_loss(child, method_name, value_name)
